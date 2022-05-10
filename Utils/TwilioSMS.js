686576bf7e198ca2ffe5a138a501f0bcaef00c5c
ashishdiwedi7600 <84765253+ashishdiwedi7600@users.noreply.github.com>
2022-05-10 13:04:40 +0530

    change authToken

--- Utils/TwilioSMS.js
+++ Utils/TwilioSMS.js
@@ -1,5 +1,5 @@
 const accountSid = 'AC81a771df88dafea49f95158000290800';
-const authToken = '28d3ffe9f4a2eaaa0d028f547bdef2e2';
+const authToken = '2ccbcd75b638a0cde5f1e59a69261406';
 const client = require('twilio')(accountSid, authToken);
 
 
